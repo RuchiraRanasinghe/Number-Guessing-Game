449374f3118a72c2f82bfda978d02a38aac72867
RuchiraRanasinghe <rhvruchira@gmail.com>
2024-10-30 11:38:51 +0530

    Second Commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let randomNumber = Math.floor((Math.random()*10)+1);
 let lblChances = document.getElementById("lblChances");
 let lblResult = document.getElementById("lblResult");
 
+
+
 let count = 3;
 lblChances.innerHTML = `Chances left : ${count}`;
 
@@ -11,11 +13,14 @@ function guess(){
         let txtInput = Number(document.getElementById("txtInput").value);
         if(txtInput == randomNumber){
             lblResult.innerHTML = "You Win...!🤩";
+            
             lblChances.innerHTML = "";
         }else if(randomNumber > txtInput){
             lblResult.innerHTML = "Your Number is Too low..!🥱";
+            imgBox.innerHTML=`<img src="https://media.tenor.com/6I9cJ0kdQXIAAAAM/this-is-the-way-tom-the-cat.gif" alt="" class="mx-auto">`;
         }else{
             lblResult.innerHTML = "Your Number is Too high..!😬";
+            imgBox.innerHTML=`<img src="https://media.tenor.com/6I9cJ0kdQXIAAAAM/this-is-the-way-tom-the-cat.gif" alt="" class="mx-auto">`;
         }
         count--;
         lblChances.innerHTML = `Chances left : ${count}`;
